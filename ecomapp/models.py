ce3be403ffe886159b06e139e65ea0e6a4d9d0a3
brahim024 <ifninos168@gmail.com>
2020-09-26 18:56:25 +0200

    add absolute url

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=100)
@@ -10,6 +10,8 @@ class Category(models.Model):
         verbose_name_plural='categories'
         def __str__(self):
             return self.name
+        def get_absolute_url(self):
+            return reverse('ecomapp:product_list_by_category',args=[self.slug])
 class Product(models.Model):
     category=models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
     name=models.CharField(max_length=40,db_index=True)
@@ -25,3 +27,5 @@ class Product(models.Model):
         index_together=(('id','slug'),)
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('ecommapp:product_detail',args=[self.id,self.slug])
