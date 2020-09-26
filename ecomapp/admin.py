abb27f6553e71693a7b2df6ce48018e3564603c7
brahim024 <ifninos168@gmail.com>
2020-09-26 17:51:44 +0200

    Registering catalog models on the

--- ecomapp/admin.py
+++ ecomapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name','slug']
+    prepopulated_fields={'slug':('name',)}
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','slug','price',
+                    'available','created','updated']
+    list_filter=['available','created','updated']
+    list_editable=['price','available']
+    prepopulated_fields={'slug':('name',)}
