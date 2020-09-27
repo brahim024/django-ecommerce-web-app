07694885aeacbe696966dc404e0c4f3fd28fcf9b
brahim024 <ifninos168@gmail.com>
2020-09-28 00:46:09 +0200

    test

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -28,4 +28,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 def get_absolute_url(self):
-    return reverse('ecommapp:product_detail',args=[self.id,self.slug])
+    return reverse('ecomapp:product_detail',args=[self.id,self.slug])
