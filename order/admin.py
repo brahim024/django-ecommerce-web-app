ac8cbeae5e512c3775f2efe6d8ca5e90834b4d63
brahim024 <ifninos168@gmail.com>
2020-09-29 20:50:19 +0200

    register models in admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Order, OrderItem
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    raw_id_fields=['product']
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','first_name','last_name','email','address',
+                'code_postal','city','paid','created','updated']
+    list_filter=['paid','created','updated']
+    inlines=[OrderItemInline]
--- order/models.py
+++ order/models.py
@@ -19,10 +19,10 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.item.all())
 
 
-class OrederItem(models.Model):
+class OrderItem(models.Model):
     order=models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
     product=models.ForeignKey(Product,related_name='order_items',on_delete=models.CASCADE)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     quantity=models.PositiveIntegerField(default=1)
-    def __str__(self):
+    def get_cost(self):
         return self.price * self.quantity
