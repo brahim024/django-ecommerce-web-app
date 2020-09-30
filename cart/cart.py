f5e167777c529bb1f946ce3cf88ba626f252f67c
brahim024 <ifninos168@gmail.com>
2020-09-30 23:23:18 +0200

    add clear page

--- cart/cart.py
+++ cart/cart.py
@@ -43,5 +43,5 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity']for item in self.cart.values())
 
     def clear(self):
-        del self.session[setting.CART_SESSION_ID]
+        del self.session[settings.CART_SESSION_ID]
         self.save()
