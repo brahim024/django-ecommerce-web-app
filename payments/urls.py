f89a17e879da6365c451b9b4fa91ea3d53296111
brahim024 <ifninos168@gmail.com>
2020-10-09 16:58:05 +0200

    add payment done and canceled

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from django.urls import  path
+from . import views
+
+app_name='payments'
+
+urlpatterns=[
+		path('process/',views.process,name='process'),
+		path('done/',views.done,name='done'),
+		path('canceled/',views.canceled,name='canceled')
+]
