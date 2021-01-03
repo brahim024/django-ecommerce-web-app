0f7b62fdce9b30eedcc2b6b77725ca88f2d83b81
brahim024 <ifninos168@gmail.com>
2021-01-03 23:19:31 +0100

    fix form busg

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
+from django.http import HttpResponse
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 from .forms import CommentForm
+
 # Create your views here.
 def product_list(request,category_slug=None):
     category=None
@@ -11,22 +13,20 @@ def product_list(request,category_slug=None):
         category=get_object_or_404(Category,slug=category_slug)
         product=product.filter(category=category)
     return render(request,'list.html',
-                            {'category':category,
+                          {'category':category,
                             'categories':categories,
                             'products':product})
-
 def product_detail(request,id,slug):
     product=get_object_or_404(Product,id=id,slug=slug,available=True)
     cart_product_form=CartAddProductForm()
-    commnts=product.comment.filter(active=True)
-    new_comment=None
-    if request.mesthod=="POST":
-        comment_form=CommentForm(data=requests.POST)
-        if comment_form.is_valid():
-            new_comment=comment_form.save(commit=False)
+    if request.method=='POST':
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            new_comment=form.save(commit=False)
             new_comment.product=product
             new_comment.save()
+            return HttpResponse('your comment craeted success!')
         else:
-            comment_form=CommentForm()
-    return render(request,'details.html',{'product':product,'cart_product_form':cart_product_form,
-                            'commnts':commnts,'comment_form':comment_form})
+            form=CommentForm()
+    context={'form':form,'product':product,'cart_product_form':cart_product_form}
+    return render(request,'details.html',context)
