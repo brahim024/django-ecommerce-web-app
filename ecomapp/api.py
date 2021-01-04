8957e7bda17d9a0f585c66b20242da16a8e5379e
brahim024 <ifninos168@gmail.com>
2021-01-04 14:08:44 +0100

    add api generate product data

--- ecomapp/api.py
+++ ecomapp/api.py
@@ -1,1 +1,15 @@
-api.py
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
+@api_view(['GET'])
+def product_list_api(request):
+	all_product=Product.objects.all()
+	data=ProductSerializer(all_product,many=True).data
+	return Response({'data':data})
+def product_detail(request,id):
+	product_detail=Product.get(id=id)
+	data=ProductSerializer(product_detail).data
+	return Response({'data':data})
